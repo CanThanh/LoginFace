0f36c591be2ef9e66372190576151c6ecee2d16a
Cấn Văn Thành <thanhcv@vnpt.vn>
2022-09-05 17:52:39 +0700

    Get Code mail

--- XMDT/Form1.cs
+++ XMDT/Form1.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -53,7 +54,7 @@ namespace XMDT
             //var code = mail.GetCode(inf.Email, inf.PassMail, "outlook.office365.com", 993, @"\d+");
 
             string chromeDriverPath = currentDirectory + "\\Library";
-            
+
             var options = new ChromeOptions();
             options.AddArgument("no-sandbox");
             //options.AddArgument("--proxy-server=http://" + proxy);
@@ -62,7 +63,7 @@ namespace XMDT
             proxyQA.HttpProxy = proxy;
             options.Proxy = proxyQA;
             options.AddArgument("--user-agent=" + userAgent);
-            ChromeDriver driver = new ChromeDriver(chromeDriverPath, options, TimeSpan.FromDays(20)); 
+            ChromeDriver driver = new ChromeDriver(chromeDriverPath, options, TimeSpan.FromDays(20));
 
             string url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1656739386&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d7426e649-3fef-467b-6e2b-6026c58ea03b&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015";
             string urlFace = "https://m.facebook.com/login/?ref=dbl&fl";
@@ -74,7 +75,7 @@ namespace XMDT
 
             //LoginFace(driver, urlFace, inf.Id, inf.Pass, inf.TwoFA );
             //var data = GetFaceInfo(driver, urlFaceInfo, inf.Id);
-            GetCodeHotMail(driver, "https://outlook.live.com/mail/0/", inf.Email, inf.PassMail);
+            GetCodeHotMail(driver, url, inf.Email, inf.PassMail);
         }
 
         private FaceInfo GetFaceInfo(ChromeDriver driver, string url, string userId)
@@ -125,7 +126,7 @@ namespace XMDT
             var about = document.DocumentNode.SelectSingleNode("//div[@id='bio']//div[@class='_5cds _2lcw _5cdt']");
             result.About = about.InnerText;
 
-            var  favourite = document.DocumentNode.SelectSingleNode("//div[@id='quote']//div[@class='_5cds _2lcw _5cdt']");
+            var favourite = document.DocumentNode.SelectSingleNode("//div[@id='quote']//div[@class='_5cds _2lcw _5cdt']");
             result.Favourite = favourite.InnerText;
 
             var friends = document.DocumentNode.SelectNodes("//div[@id='profile-card']//div[@class='title mfsm fcb']");
@@ -176,9 +177,9 @@ namespace XMDT
             }
         }
 
-        private string GetCodeHotMail(ChromeDriver driver, string url, string user, string pass)
+        private List<string> GetCodeHotMail(ChromeDriver driver, string url, string user, string pass)
         {
-            string result = "";
+            List<string> result =new List<string>();
             driver.Url = url;
             driver.Navigate().GoToUrl(url);
             driver.FindElement(By.XPath("//input[@name='loginfmt']")).SendKeys(user);
@@ -189,23 +190,44 @@ namespace XMDT
             Thread.Sleep(1000);
             driver.FindElement(By.XPath("//input[@id='idSIButton9']")).Click();
             Thread.Sleep(3000);
-            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-            //IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            //wait.Until(wd => js.ExecuteScript("return document.readyState").ToString() == "complete");
-            var innerHtml = driver.PageSource;
-            var html = driver.FindElement(By.XPath("html")).GetInnerHTML();
-            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
-            document.LoadHtml(html);
-            var itemnodes = document.DocumentNode.SelectNodes("//div[@class='customScrollBar BW6l9']//div[@class='jA_gm']");
-            foreach (var item in itemnodes)
+            //var waitForJStoLoad = false;
+            //while (!waitForJStoLoad)
+            //{
+            //    waitForJStoLoad = WaitForJStoLoad(driver);
+            //}
+            var buttons = driver.FindElements(By.XPath("//button[@role= 'tab']"));
+            foreach (var button in buttons)
             {
-                HtmlAgilityPack.HtmlDocument documentItem = new HtmlAgilityPack.HtmlDocument();
-                documentItem.LoadHtml(item.InnerHtml);
-                var title = documentItem.DocumentNode.SelectSingleNode("//div[contains(@class,'Lf0qr')]//span").InnerText;
-                var abcd2 = documentItem.DocumentNode.SelectSingleNode("//span[@class='JLM0t MtC_r jYIx5']").InnerText;
-            }
+                button.Click();
+                Thread.Sleep(1000);
+                driver.ExecuteScript("window.scrollTo(0, 10);");
+                Thread.Sleep(1000);
+                var itemnodes = driver.FindElements(By.XPath("//div[contains(@class,'customScrollBar')]//div[@role='option']"));
+                foreach (var item in itemnodes)
+                {
+                    var temp = item.GetInnerHTML();
+                    var text = item.Text;
+                    if (text.ToLower().Contains("facebook"))
+                    {
+                        var code = Regex.Match(text, @"\d{6,8}").Value;
+                        if (code.Length >= 6)
+                        {
+                            result.Add(code);
+                        }
+                    }
+                }
+            }        
             return result;
+        }
+
+        private bool WaitForJStoLoad(WebDriver driver)
+        {
+            //bool result = true;
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 120));
+            //result = result && wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return jQuery.active").Equals(0));
+            //result = result && wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+            //return result;
+            return wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")); 
         }
 
         private string GetData(string url, HttpRequest http = null, string userArgent = "", string cookie = null)
@@ -240,6 +262,6 @@ namespace XMDT
                     http.Cookies.Add(temp2[0], temp2[1]);
                 }
             }
-        }
+        }               
     }
 }
