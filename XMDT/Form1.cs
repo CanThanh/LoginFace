b56266ecea5011bbd71f71dd72f877cb8fadbd9a
thanhcv <thanhcv@vnpt.vn>
2023-02-08 09:32:19 +0700

    Get code email

--- XMDT/Form1.cs
+++ XMDT/Form1.cs
@@ -59,9 +59,8 @@ namespace XMDT
             inf.PassMail = infAccount[4];
             //inf.EmailRecover = infAccount[5];
 
-            //Mail mail = new Mail();
-            //var code = mail.GetCode(inf.Email, inf.PassMail, "outlook.office365.com", 993, @"\d+");
-            //string chromeDriverPath = currentDirectory + "\\Library\\chromedriver_win32";
+            Mail mail = new Mail();
+            var code = mail.GetCode(inf.Email, inf.PassMail, "outlook.office365.com", 993, @"\d+");
 
             var driver = InitChromeDriver();
 
@@ -70,26 +69,29 @@ namespace XMDT
             string urlAddMailFace = "https://m.facebook.com/ntdelegatescreen/?params=%7B%22entry-point%22%3A%22settings%22%7D&path=%2Fcontacts%2Fmanagement%2F";
             string urlFaceInfo = "https://m.facebook.com/profile.php?v=info&_rdr";
 
-            string cookie = "presence=EDvF3EtimeF1675742421EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC;cppo=1;m_page_voice=100003574129834;xs=10%3AAA9MUI7T-hnOGg%3A2%3A1675742409%3A-1%3A11945;c_user=100003574129834;locale=vi_VN;wd=929x879;m_pixel_ratio=1;fr=05YnzmcTSx7a2u5xz.AWUqjLht6jYAmSiSj46xHsf97qg.Bj4cy5.7Q.AAA.0.0.Bj4czH.AWVw7EQ79Wg;usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3o0aDQwdno4MyIsInRpbWUiOjE2NzU3NDI0MTd9;sb=uczhYxqd-jNUJ7QzKES0Nu_B;datr=uczhY2GY8sB6sc4tUfXL3Lgj";
+            //string cookie = "presence=EDvF3EtimeF1675742421EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC;cppo=1;m_page_voice=100003574129834;xs=10%3AAA9MUI7T-hnOGg%3A2%3A1675742409%3A-1%3A11945;c_user=100003574129834;locale=vi_VN;wd=929x879;m_pixel_ratio=1;fr=05YnzmcTSx7a2u5xz.AWUqjLht6jYAmSiSj46xHsf97qg.Bj4cy5.7Q.AAA.0.0.Bj4czH.AWVw7EQ79Wg;usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3o0aDQwdno4MyIsInRpbWUiOjE2NzU3NDI0MTd9;sb=uczhYxqd-jNUJ7QzKES0Nu_B;datr=uczhY2GY8sB6sc4tUfXL3Lgj";
 
-            string cookie2 = "datr=1LvhY1pGpki2WZzFaXc-8f_v; sb=1LvhY8qSzVz63ESQJCNrjd0F; m_pixel_ratio=1; locale=vi_VN; fr=0Pgf72nltOesLADqP.AWVXDWEUwUp1p8bY1yjfnz6a6UU.Bj4bvU.5z.AAA.0.0.Bj4bvi.AWWjB2-UB7Q; c_user=100003574129834; xs=44%3ATITPFBpdERQNnA%3A2%3A1675738084%3A-1%3A11945; m_page_voice=100003574129834; cppo=1; usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3hyaTE2ZXJ3MWYiLCJ0aW1lIjoxNjc1NzQwNzM1fQ%3D%3D; presence=EDvF3EtimeF1675740790EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC; wd=1920x372";
+            //string cookie2 = "datr=1LvhY1pGpki2WZzFaXc-8f_v; sb=1LvhY8qSzVz63ESQJCNrjd0F; m_pixel_ratio=1; locale=vi_VN; fr=0Pgf72nltOesLADqP.AWVXDWEUwUp1p8bY1yjfnz6a6UU.Bj4bvU.5z.AAA.0.0.Bj4bvi.AWWjB2-UB7Q; c_user=100003574129834; xs=44%3ATITPFBpdERQNnA%3A2%3A1675738084%3A-1%3A11945; m_page_voice=100003574129834; cppo=1; usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3hyaTE2ZXJ3MWYiLCJ0aW1lIjoxNjc1NzQwNzM1fQ%3D%3D; presence=EDvF3EtimeF1675740790EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC; wd=1920x372";
 
-            // LoginFace(driver, urlFace, inf.Id, inf.Pass, inf.TwoFA );
-            LoginCookie(driver, cookie2);
-            string eaab = GetEAABToken(driver);
-            string dtsg = GetDTSGToken(driver);
+            //LoginFace(driver, urlFace, inf.Id, inf.Pass, inf.TwoFA );
+            ////LoginCookie(driver, cookie2);
+            //string eaab = GetEAABToken(driver);
+            //string dtsg = GetDTSGToken(driver);
 
             //string cookie = GetCookie(driver);
-            string eaag = GetEAAGTokenApi(inf.Id, cookie2);
 
-            var hoangnv1 = "hoangnv1";
-            //string eaag = GetEAAGToken(driver, inf.TwoFA);
+
+            ////string eaag = GetEAAGToken(driver, inf.Id, inf.TwoFA, dtsg, cookie);
+            //CallAuthenTwoFa(inf.Id, inf.TwoFA, dtsg, cookie);
+            //string eaag = GetEAAGTokenApi(inf.Id, cookie);
 
             //GetFaceInfo(driver, urlFaceInfo, inf.Id);
             //WriteFile(currentDirectory + "\\File\\userInfo.json");
             //ReadFile(currentDirectory + "\\File\\userInfo.json");
 
             //GetCodeHotMail(driver, urlMail, inf.Email, inf.PassMail);
+
+            var hoangnv1 = "hoangnv1";
         }
 
         private ChromeDriver InitChromeDriver()
@@ -121,7 +123,7 @@ namespace XMDT
         {
             Random random = new Random();
             var randomValue = random.NextDouble();
-            if(randomValue < 0.9)
+            if (randomValue < 0.9)
             {
                 foreach (var item in key)
                 {
@@ -132,7 +134,7 @@ namespace XMDT
             else
             {
                 var randomIndex = random.Next(key.Length - 1);
-                for(int i = 0; i < randomIndex; i++)
+                for (int i = 0; i < randomIndex; i++)
                 {
                     driver.FindElement(By.XPath(xPath)).SendKeys(key[i].ToString());
                     Thread.Sleep(50);
@@ -166,12 +168,12 @@ namespace XMDT
             var existUser = faceInfos.FirstOrDefault(x => x.Id == userId);
             if (existUser != null && !isUpdate)
             {
-                return ;
+                return;
             }
 
             var works = document.DocumentNode.SelectNodes("//div[@id='work']//div[@class='_c3u']//span");
-            if(works != null)
-            {                
+            if (works != null)
+            {
                 foreach (var item in works)
                 {
                     result.Works.Add(item.InnerText);
@@ -220,7 +222,7 @@ namespace XMDT
             }
 
             var familyMembers = document.DocumentNode.SelectNodes("//div[@id='family']//div[@class='title mfsm fcb']");
-            if(familyMembers!= null)
+            if (familyMembers != null)
             {
                 foreach (var item in familyMembers)
                 {
@@ -233,7 +235,7 @@ namespace XMDT
             {
                 result.About = about.InnerText;
             }
-            
+
             var favourite = document.DocumentNode.SelectSingleNode("//div[@id='quote']//div[@class='_5cds _2lcw _5cdt']");
             if (favourite != null)
             {
@@ -279,7 +281,6 @@ namespace XMDT
             driver.Url = url;
             driver.Navigate().GoToUrl(url);
             Thread.Sleep(1000);
-            //driver.FindElement(By.XPath("//input[@name='email']")).SendKeys(user);
             SendKeyByXPath(driver, "//input[@name='email']", user);
             driver.FindElement(By.XPath("//input[@name='pass']")).SendKeys(pass);
             Thread.Sleep(1000);
@@ -332,42 +333,44 @@ namespace XMDT
         }
 
         #region GetToken
-        private string GetEAAGToken(ChromeDriver driver, string twoFA)
+        private string GetEAAGToken(ChromeDriver driver, string user, string twoFA, string dtsg, string cookie)
         {
             //Vào trang business lấy token
             driver.Url = "https://business.facebook.com/content_management/";
             Thread.Sleep(3000);
-            string faCode = new Totp(Base32Encoding.ToBytes(twoFA)).ComputeTotp();
-            var sendFaCode = driver.FindElement(By.XPath("//input[@name='approvals_code']"));
-            if (sendFaCode != null)
-            {
-                sendFaCode.SendKeys(faCode);
-                Thread.Sleep(2000);
-            }
+            var respone = CallAuthenTwoFa(user, twoFA, dtsg, cookie);
+            Thread.Sleep(5000);
+            driver.Url = "https://business.facebook.com/content_management/";
+            Thread.Sleep(10000);
             string source = driver.PageSource;
-            string token = "EAAG" + source.Split(new[] { "EAAG" }, StringSplitOptions.None)[1].Split('"')[0];
+            string token = "EAAG";
+            if (source.Contains("EAAG"))
+            {
+                token += source.Split(new[] { "EAAG" }, StringSplitOptions.None)[1].Split('"')[0];
+            }
             return token;
+        }
+
+        private string CallAuthenTwoFa(string user, string twoFA, string dtsg, string cookie)
+        {
+            string faCode = new Totp(Base32Encoding.ToBytes(twoFA)).ComputeTotp();
+            HttpRequest httprequest = new HttpRequest();
+            AddCookie(httprequest, cookie);
+            var data = "approvals_code=" + faCode + "&save_device=false&__user=" + user + "&__a=1&__dyn=7xeUmF3EfXpUS2q3mbwyyVuC2-m2q3Kq2i5Uf9E6C7UW3q5UgDxW4E8U6ydwJwCwmUO0BoiyEqx68w9q15wfO1YCwjHwuk9wgovyolwuEsxe687C2m3K2y1nUS0jG12KdwnU5W0IU9kbxR12ewi85W1bxq1uG3G48W2a5Ey19zUcE28wdq1iwKwHxa1XwnFU2DxiaBw4kxa4UCfwSyES2e0UE2ZwiU8U6C0yEco5C&__csr=&__req=8&__hs=19395.BP%3ADEFAULT.2.0.0.0.0&dpr=1.5&__ccg=GOOD&__rev=1006920906&__s=6hjwrm%3A3vhtq1%3Aspfsji&__hsi=7197324973317924624&__comet_req=0&fb_dtsg=" + dtsg + "&jazoest=25525&lsd=Q7Feqpz-TcfK27ydcQVNhN&__aaid=191881262750333&__spin_r=1006920906&__spin_b=trunk&__spin_t=1675757806&__jssesw=1";
+            var response = httprequest.Post("https://business.facebook.com/security/twofactor/reauth/enter/", data, "application/x-www-form-urlencoded; charset=UTF-8").ToString();
+            return response;
         }
 
         private string GetEAAGTokenApi(string user, string cookie)
         {
-            //HttpRequest request = new HttpRequest();           
-            //AddCookie(request, cookie);
-            //var respone = request.Get("https://business.facebook.com/content_management/");
-            //var kq = respone.ToString();
-            //string token = "EAAG"  + kq.Split(new[] { "EAAG" }, StringSplitOptions.None)[1].Split('"')[0];
-            //return token;
-
-            var options = new RestClientOptions("https://business.facebook.com")
+            HttpRequest request = new HttpRequest();
+            AddCookie(request, cookie);
+            var respone = request.Get("https://business.facebook.com/content_management/").ToString();
+            string token = "EAAG";
+            if (respone.Contains("EAAG"))
             {
-                MaxTimeout = -1,
-                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-            };
-            var client = new RestClient(options);
-            var request = new RestRequest("https://business.facebook.com/content_management/", Method.Get);
-            request.AddHeader("cookie", cookie);
-            var respone = client.Execute(request);
-            string token = "EAAG"  + respone.Content.Split(new[] { "EAAG" }, StringSplitOptions.None)[1].Split('"')[0];
+                token += respone.Split(new[] { "EAAG" }, StringSplitOptions.None)[1].Split('"')[0];
+            }
             return token;
         }
         private string GetEAABToken(ChromeDriver driver)
@@ -376,7 +379,7 @@ namespace XMDT
             driver.Url = "https://www.facebook.com/pe";
             Thread.Sleep(2000);
             string source = driver.PageSource;
-            string token = "EAAB" + source.Split(new [] {"EAAB"}, StringSplitOptions.None)[1].Split('"')[0];
+            string token = "EAAB" + source.Split(new[] { "EAAB" }, StringSplitOptions.None)[1].Split('"')[0];
             return token;
         }
 
@@ -390,30 +393,26 @@ namespace XMDT
 
         private List<string> GetCodeHotMail(ChromeDriver driver, string url, string user, string pass)
         {
-            List<string> result =new List<string>();
+            List<string> result = new List<string>();
             driver.Url = url;
             driver.Navigate().GoToUrl(url);
             driver.FindElement(By.XPath("//input[@name='loginfmt']")).SendKeys(user);
             driver.FindElement(By.XPath("//input[@id='idSIButton9']")).Click();
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             driver.FindElement(By.XPath("//input[@name='passwd']")).SendKeys(pass);
             driver.FindElement(By.XPath("//input[@id='idSIButton9']")).Click();
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             driver.FindElement(By.XPath("//input[@id='idSIButton9']")).Click();
             Thread.Sleep(3000);
-            //var waitForJStoLoad = false;
-            //while (!waitForJStoLoad)
-            //{
-            //    waitForJStoLoad = WaitForJStoLoad(driver);
-            //}
-            var buttons = driver.FindElements(By.XPath("//button[@role= 'tab']"));
+
+            var buttons = driver.FindElements(By.XPath("//button[contains(@class,'ms-Pivot-link')]"));
             foreach (var button in buttons)
             {
                 button.Click();
-                Thread.Sleep(1000);
+                Thread.Sleep(2000);
                 driver.ExecuteScript("window.scrollTo(0, 10);");
                 Thread.Sleep(1000);
-                var itemnodes = driver.FindElements(By.XPath("//div[contains(@class,'customScrollBar')]//div[@role='option']"));
+                var itemnodes = driver.FindElements(By.XPath("//div[contains(@class,'hcptT gDC9O')]"));
                 foreach (var item in itemnodes)
                 {
                     var temp = item.GetInnerHTML();
@@ -427,20 +426,12 @@ namespace XMDT
                         }
                     }
                 }
-            }        
+            }
 
             return result;
         }
 
-        private bool WaitForJStoLoad(WebDriver driver)
-        {
-            //bool result = true;
-            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 120));
-            //result = result && wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return jQuery.active").Equals(0));
-            //result = result && wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
-            //return result;
-            return wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")); 
-        }
+
 
         private string GetData(string url, HttpRequest http = null, string userArgent = "", string cookie = null)
         {
