dfe01c6a80316d19e78108f32e1f1ce40c8b6f7a
cvthanh028428@gmail.com <ThanhCV239666>
2023-03-02 15:50:56 +0700

    sqLite

---
+++ XMDT/Controller/SQLiteProcessing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace XMDT.Controller
+{
+    internal class SQLiteProcessing
+    {
+        string _strConnect = "Data Source=db_facebook.sqlite;";
+        SQLiteConnection _con = new SQLiteConnection();
+        public void createConection()
+        {
+            _con.ConnectionString = _strConnect;
+            _con.Open();
+        }
+
+        public void closeConnection()
+        {
+            _con.Close();
+        }
+
+        public void createTable()
+        {
+            createConection();
+            string sql = "CREATE TABLE IF NOT EXISTS FILES (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " Name TEXT, CreatedDate TEXT, Active INT)"; 
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
+            command.ExecuteNonQuery();
+            sql = "CREATE TABLE IF NOT EXISTS ACCOUNTS (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " Info TEXT)";
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+            sql = "CREATE TABLE IF NOT EXISTS INTERACTS (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " UId TEXT, TimeInteract TEXT, Action TEXT, Config TEXT)";
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+            closeConnection();
+        }
+
+        public void insertDataTest()
+        {
+            createConection();
+            string sql = "INSERT INTO FILES(Name, CreatedDate, Active) VALUES('BTH','" + DateTime.Now.ToString() + "', 1)";
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
+            command.ExecuteNonQuery();
+            closeConnection();
+        }
+    }
+}
