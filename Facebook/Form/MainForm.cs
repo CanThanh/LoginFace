5254e609de4c9e34eab790183005e9ae95ade8af
cvthanh028428@gmail.com <ThanhCV239666>
2023-03-29 14:26:00 +0700

    Login face

--- Facebook/Form/MainForm.cs
+++ Facebook/Form/MainForm.cs
@@ -34,25 +34,9 @@ namespace Facebook
             InitComboBoxFile();
             LoadDataGridView();
             KeyResovelCatcha = "90b9de403cd4c42f45a4f9048760dec0";
-            apiKey = "172c280613d44fc194b2143054690b7e";
+            apiKey = "172c280613d44fc194b2143054690b7e"; //otp sell
+            //apiKey = "172c280613d44fc194b2143054690b7e"; //doi sang chothuesimcode
         }
-
-        //private async Task<string> TestApi()
-        //{
-        //    string url = "https://www.google.com/recaptcha/api2/payload?p=06AKH6MRF4E5skjweT2payPoq_AV8W1ykmcxqOpWU32iuqSH1RKYOyVn6Lt__N-j3cNa2o3VmL8fs8kC2Yay9_Wr1MNY4zAqE-uP5qvRE9nDeflD0Z2ETXKCXvF9gtXlItM0U4TARIOe06T1oYzx_NbmImjgfTCO4pQZWTho9GUCUHQtl1M9celXW3MyO7U21j2HDZrjaAUvu2zpePRueBCxFaKUyRhDK29mGp0uNYMPPQKJMPqoegaIc&k=6Lc9qjcUAAAAADTnJq5kJMjN9aD1lxpRLMnCS2TR";
-        //    var options = new RestClientOptions("")
-        //    {
-        //        MaxTimeout = -1,
-        //    };
-        //    var client = new RestClient(options);
-        //    var request = new RestRequest("http://8b22-35-204-124-172.ngrok.io/transcribe", Method.Post);
-        //    request.AlwaysMultipartFormData = true;
-        //    request.AddFile("file", url);
-        //    RestResponse response = await client.ExecuteAsync(request);
-        //    MessageBox.Show(response.Content);
-        //    return response.Content;
-        //}
-
         #region InitData when Form Load
         private void InitComboBoxFile()
         {
@@ -69,84 +53,9 @@ namespace Facebook
         }
         #endregion
 
-        //100007557514409  snowkvt123  B6R546Q2K26FCFOTLU2MUKT3ANYWLRYY   micshidevon@hotmail.com     @thainguyenteam@@1022020    micshidevon2022 @getnada.com
-        //100009306396626  snowkvt1234  76IZ6TLE4XTKIWFRIKCVQSWYSRMZRWQN	alishauemargie@hotmail.com  @thainguyenteam@@1022020    alishauemargie2022@getnada.com
-        private void btnProgess_Click()
-        {
-            string currentDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory)));
-            FileHelper fileHelper = new FileHelper();
-            var pathUserAgent = currentDirectory + "\\File\\ua.txt";
-            var userAgent = fileHelper.ReadLine(pathUserAgent)[Rand.Next(0, fileHelper.ReadLine(pathUserAgent).Count)];
-            AccountInfo account = new AccountInfo();
-
-            var pathProxy = currentDirectory + "\\File\\proxy.txt";
-            var proxy = fileHelper.ReadLine(pathProxy)[Rand.Next(0, fileHelper.ReadLine(pathProxy).Count)];
-
-            var pathAccount = currentDirectory + "\\File\\account.txt";
-            var accountLst = fileHelper.ReadLine(pathAccount)[Rand.Next(0, fileHelper.ReadLine(pathAccount).Count)];
-
-            var pathSecondaryEmail = currentDirectory + "\\File\\secondaryemail.txt";
-            var strSecondaryEmail = fileHelper.ReadLine(pathSecondaryEmail)[Rand.Next(0, fileHelper.ReadLine(pathSecondaryEmail).Count)];
-            var secondaryEmail = new SecondaryEmail();
-            var infSecondaryEmail = strSecondaryEmail.Split('|');
-            secondaryEmail.Email = infSecondaryEmail[0];
-            secondaryEmail.Pass = infSecondaryEmail[1];
-
-            var infAccount = accountLst.Split(' ');
-            account.Id = infAccount[0];
-            account.Pass = infAccount[1];
-            account.TwoFA = infAccount[2];
-            account.Email = infAccount[3];
-            account.PassMail = infAccount[4];
-            //inf.EmailRecover = infAccount[5];
-
-            //MailProcessing mail = new MailProcessing();
-            //var code = mail.GetCode(inf.Email, inf.PassMail, "outlook.office365.com", 993, @"\d+");
-
-            FacebookProcessing facebookProcessing = new FacebookProcessing();
-            //var kq = facebookProcessing.GetLinkFaceImage(30, "male");
-
-            var driver = facebookProcessing.InitChromeDriver(account);
-
-            string urlMail = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1656739386&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d7426e649-3fef-467b-6e2b-6026c58ea03b&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015";
-            string urlFace = "https://m.facebook.com/login/?ref=dbl&fl";
-            string urlAddMailFace = "https://m.facebook.com/ntdelegatescreen/?params=%7B%22entry-point%22%3A%22settings%22%7D&path=%2Fcontacts%2Fmanagement%2F";
-            string urlFaceInfo = "https://m.facebook.com/profile.php?v=info&_rdr";
-
-            //string cookie = "presence=EDvF3EtimeF1675742421EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC;cppo=1;m_page_voice=100003574129834;xs=10%3AAA9MUI7T-hnOGg%3A2%3A1675742409%3A-1%3A11945;c_user=100003574129834;locale=vi_VN;wd=929x879;m_pixel_ratio=1;fr=05YnzmcTSx7a2u5xz.AWUqjLht6jYAmSiSj46xHsf97qg.Bj4cy5.7Q.AAA.0.0.Bj4czH.AWVw7EQ79Wg;usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3o0aDQwdno4MyIsInRpbWUiOjE2NzU3NDI0MTd9;sb=uczhYxqd-jNUJ7QzKES0Nu_B;datr=uczhY2GY8sB6sc4tUfXL3Lgj";
-
-            //string cookie2 = "datr=1LvhY1pGpki2WZzFaXc-8f_v; sb=1LvhY8qSzVz63ESQJCNrjd0F; m_pixel_ratio=1; locale=vi_VN; fr=0Pgf72nltOesLADqP.AWVXDWEUwUp1p8bY1yjfnz6a6UU.Bj4bvU.5z.AAA.0.0.Bj4bvi.AWWjB2-UB7Q; c_user=100003574129834; xs=44%3ATITPFBpdERQNnA%3A2%3A1675738084%3A-1%3A11945; m_page_voice=100003574129834; cppo=1; usida=eyJ2ZXIiOjEsImlkIjoiQXJwb3hyaTE2ZXJ3MWYiLCJ0aW1lIjoxNjc1NzQwNzM1fQ%3D%3D; presence=EDvF3EtimeF1675740790EuserFA21B03574129834A2EstateFDutF0CEchF_7bCC; wd=1920x372";
-
-            //facebookProcessing.LoginCookie(driver, cookie);
-
-            facebookProcessing.LoginFace(driver, urlFace, account.Id, account.Pass, account.TwoFA);
-            string eaab = facebookProcessing.GetEAABToken(driver);
-            string dtsg = facebookProcessing.GetDTSGToken(driver);
-            string cookie = CommonFunction.GetCookie(driver);
-            facebookProcessing.CallAuthenTwoFa(account.Id, account.TwoFA, dtsg, cookie);
-            Thread.Sleep(2000);
-            string eaag = facebookProcessing.GetEAAGTokenApi(account.Id, cookie);
-
-            //var urlInfo = "https://graph.facebook.com/v15.0/me?access_token="+ eaag +"&fields=id%2Cname%2Cfirst_name%2Cgender%2Chometown%2Crelationship_status%2Creligion%2Cfriends%2Cbirthday%2Clast_name";
-            //cookie = facebookProcessing.GetCookie(driver);
-
-            //var kq = facebookProcessing.GetUserInfo(inf.Id);
-
-            var kq = facebookProcessing.GetUserInfoSecond(eaag, cookie);
-
-            var x = CommonFunction.UnicodeToUTF8(kq);
-
-            //GetFaceInfo(driver, urlFaceInfo, inf.Id);
-            //WriteFile(currentDirectory + "\\File\\userInfo.json");
-            //ReadFile(currentDirectory + "\\File\\userInfo.json");
-
-            //GetCodeHotMail(driver, urlMail, inf.Email, inf.PassMail);
-
-            var hoangnv1 = "hoangnv1";
-        }
-
         private void configXMDT_Click(object sender, EventArgs e)
         {
+            //Chỉnh lại cấu hình phôi chỗ này có thể cho load từ db
             ConfigImage configImage = new ConfigImage("1");
             configImage.Show();
         }
@@ -405,7 +314,16 @@ namespace Facebook
         #region Login
         private void cmsLogin_Click(object sender, EventArgs e)
         {
-
+            dgViewInput.ContextMenuStrip.Close();
+            lstRowDataRun.Clear();
+            foreach (DataGridViewRow item in dgViewInput.Rows)
+            {
+                if (Convert.ToBoolean(item.Cells["colCheck"].Value))
+                {
+                    lstRowDataRun.Add(dgViewInput.Rows.IndexOf(item));
+                }
+            }
+            RunMultiThread(FacebookLogin);
         }
         private void FacebookLogin(int rowIndex)
         {
@@ -413,12 +331,20 @@ namespace Facebook
             FacebookProcessing facebookProcessing = new FacebookProcessing();
             var account = lstAccountInfo[rowIndex];
             var result = false;
+            var driver = facebookProcessing.InitChromeDriver(account);
             if (rbLoginCookie.Checked)
             {
-                //facebookProcessing.LoginCookie(account);
+                facebookProcessing.LoginCookie(driver, FacebookLinkUrl.MFacebook, account.Cookie);
             }else if (rbLoginUP.Checked)
             {
-
+                if (string.IsNullOrEmpty(account.TwoFA))
+                {
+                    facebookProcessing.LoginFace(driver, FacebookLinkUrl.MFacebook, account.Id, account.Pass);
+                }
+                else
+                {
+                    facebookProcessing.LoginFace(driver, FacebookLinkUrl.MFacebook, account.Id, account.Pass, account.TwoFA);
+                }
             }
 
             dgViewInput.Rows[rowIndex].Cells["colStatus"].Value = result ? "Thành công" : "Lỗi";
